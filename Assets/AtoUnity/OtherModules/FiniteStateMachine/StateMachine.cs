d870402065b761268dcf35aaa717e1043b02bf63
HuyLM <huylm@ftech.ai>
2023-03-27 17:27:59 +0700

    doing FSM

---
+++ Assets/AtoUnity/OtherModules/FiniteStateMachine/StateMachine.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AtoGame.OtherModules.FSM
+{
+    public abstract class StateMachine<T> where T : IContext
+    {
+        protected T context;
+        State<T> currentState;
+        protected List<Transition<T>> anyStateTransitions;
+
+
+        public T Context { get => context; }
+
+        public virtual void Initialize()
+        {
+
+        }
+
+        public void Updating()
+        {
+            // Do Always Actions
+            DoAlwaysActions();
+            // Transition From Any States
+            CheckTransitionFromAnyStates();
+            // Update Current State
+            currentState?.UpdateState(this);
+        }
+
+        protected abstract void DoAlwaysActions();
+        protected void CheckTransitionFromAnyStates()
+        {
+            if (anyStateTransitions == null)
+            {
+                return;
+            }
+            for (int i = 0; i < anyStateTransitions.Count; ++i)
+            {
+                if (anyStateTransitions[i].CheckTransition(this))
+                {
+                    return;
+                }
+            }
+        }
+
+        public void TransitionToState(State<T> nextState, Transition<T> transition)
+        {
+            if (nextState != null && nextState != currentState && currentState != null)
+            {
+                transition.DoBeforeTransitionActions(this);
+                currentState.EndState(this);
+                transition.DoWhileTransitionActions(this);
+                SetCurrentState(nextState);
+                currentState.StartState(this);
+                transition.DoAfterTransitionActions(this);
+            }
+        }
+
+        protected void SetCurrentState(State<T> currentState)
+        {
+            this.currentState = currentState;
+        }
+
+        void OnDrawGizmos(Transform transform)
+        {
+            if (currentState != null)
+            {
+                Gizmos.color = currentState.SceneGizmoColor;
+                Gizmos.DrawWireSphere(transform.position, 0.5f);
+            }
+        }
+    }
+}
