b5966bbbef192a621358b8c189974acadfbdc78e
Huylm <>
2023-12-12 15:53:03 +0700

    add public to jsonDatetime

--- Assets/AtoUnity/Base/Runtime/Helper/SaveLoad/JsonDateTime.cs
+++ Assets/AtoUnity/Base/Runtime/Helper/SaveLoad/JsonDateTime.cs
@@ -5,23 +5,23 @@ using System;
 namespace AtoGame.Base.Helper
 {
     [Serializable]
-    struct JsonDateTime
+    public struct JsonDateTime
     {
         public long v;
         public static implicit operator DateTime(JsonDateTime jdt)
         {
-            return new DateTime(jdt.v);
+            return DateTime.FromBinary(jdt.v);
         }
         public static implicit operator JsonDateTime(DateTime dt)
         {
             JsonDateTime jdt = new JsonDateTime();
-            jdt.v = dt.Ticks;
+            jdt.v = dt.ToBinary();
             return jdt;
         }
     }
 
     [Serializable]
-    struct JsonTimeSpan
+    public struct JsonTimeSpan
     {
         public long v;
 
